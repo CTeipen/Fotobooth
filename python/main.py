991d45248c7efd0fae131063b77c72881e541196
C.Teipen <info@cteipen.de>
2019-02-20 20:17:21 +0100

    Edit Main Python application

--- python/main.py
+++ python/main.py
@@ -20,6 +20,7 @@ def browse_button():
 def choose_cloud_folder():
     global cloudFolder
     cloudFolder.set(browse_button())
+    print()
 
 
 def choose_usb_folder():
@@ -27,23 +28,27 @@ def choose_usb_folder():
     usbFolder.set(browse_button())
 
 def check_vars():
-    if len(txt_cloudFolder.get()) > 0:
-        if len(txt_usbFolder.get()) > 0:
-            if len(txt_qrCode.get()) > 0:
-                if len(txt_zeit.get()) > 0:
-                    return True
-                else:
-                    messagebox.showinfo('Fehlende Eingabe', 'Bitte geben Sie das aktuelle Datum und die Uhrzeit im vorgegebenen Format an.')
-                    return False
-            else:
-                messagebox.showinfo('Fehlende Eingabe', 'Bitte geben Sie eine URL an, auf die der QR Code zeigen soll.')
-                return False
-        else:
-            messagebox.showinfo('Fehlende Eingabe', 'Bitte geben Sie den Pfad zum USB Stick an.')
-            return False
-    else:
+    if len(txt_cloudFolder.get()) <= 1:
         messagebox.showinfo('Fehlende Eingabe', 'Bitte geben Sie den Pfad zum You In The Cloud Ordner an.')
         return False
+
+    if bool_usbFolder.get() == 1:
+        if len(txt_usbFolder.get()) <= 1:
+            messagebox.showinfo('Fehlende Eingabe', 'Bitte geben Sie den Pfad zum USB Stick an.')
+            return False
+
+    if bool_qrCode.get() == 1:
+        if len(txt_qrCode.get()) <= 0:
+            messagebox.showinfo('Fehlende Eingabe', 'Bitte geben Sie eine URL an, auf die der QR Code zeigen soll.')
+            return False
+
+    if bool_zeit.get() == 1:
+        if len(txt_zeit.get()) != 20:
+            messagebox.showinfo('Fehlende Eingabe', 'Bitte geben Sie das aktuelle Datum und die Uhrzeit im vorgegebenen Format an.')
+            return False
+
+    return True
+
 
 
 def startScript():
@@ -61,7 +66,7 @@ def startScript():
 
         btn_start.destroy()
         btn_end = ttk.Button(window,text='Beenden', command=stopScript)
-        btn_end.grid(column=2,row=7, sticky=W)
+        btn_end.grid(row=13, column=2, sticky=E)
 
     # print('End')
 
@@ -92,7 +97,7 @@ def stopScript():
 ### Window
 window = Tk()
 window.title("Fotobox 2.0")
-window.geometry('665x220')
+window.geometry('410x360')
 
 
 ### Style
@@ -101,58 +106,78 @@ ttk.Style().configure("TButton", padding=6, relief="flat",
 
 
 ### Cloud Folder
-lbl_cloudFolder = Label(master=window,text="Cloud Folder")
-lbl_cloudFolder.grid(row=0, column=0, sticky=W)
+lbl_cloudFolder = Label(master=window,text="Cloud Folder *")
+lbl_cloudFolder.grid(row=0, column=1, sticky=W)
 
 cloudFolder = StringVar()
-txt_cloudFolder = Entry(master=window,width=50,textvariable=cloudFolder,state='disabled')
-txt_cloudFolder.grid(row=0, column=1)
+txt_cloudFolder = Entry(master=window,width=30,textvariable=cloudFolder,state='disabled')
+txt_cloudFolder.grid(row=1, column=1)
 
 btn_cloudFolder = ttk.Button(text="...", command=choose_cloud_folder)
-btn_cloudFolder.grid(row=0, column=2)
+btn_cloudFolder.grid(row=1, column=2)
 
-
-### USB Folder
-lbl_usbFolder = Label(master=window,text="USB Folder")
-lbl_usbFolder.grid(row=1, column=0, sticky=W)
-
-usbFolder = StringVar()
-txt_usbFolder = Entry(master=window,width=50,textvariable=usbFolder,state='disabled')
-txt_usbFolder.grid(row=1, column=1)
-
-btn_usbFolder = ttk.Button(text="...", command=choose_usb_folder)
-btn_usbFolder.grid(row=1, column=2)
-
-
+###
 lbl_empty = Label(master=window,text="")
 lbl_empty.grid(row=2, column=0)
 
+### USB Folder
+bool_usbFolder = IntVar()
+chb_usbFolder = Checkbutton(master=window, variable=bool_usbFolder)
+chb_usbFolder.grid(row=3, column=0)
+
+lbl_usbFolder = Label(master=window,text="USB Folder")
+lbl_usbFolder.grid(row=3, column=1, sticky=W)
+
+usbFolder = StringVar()
+txt_usbFolder = Entry(master=window,width=30,textvariable=usbFolder,state='disabled')
+txt_usbFolder.grid(row=4, column=1)
+
+btn_usbFolder = ttk.Button(text="...", command=choose_usb_folder)
+btn_usbFolder.grid(row=4, column=2)
+
+###
+lbl_empty = Label(master=window,text="")
+lbl_empty.grid(row=5, column=0)
+
 
 ### QR Code
+bool_qrCode = IntVar()
+chb_qrCode = Checkbutton(master=window, variable=bool_qrCode)
+chb_qrCode.grid(row=6, column=0)
+
 lbl_qrCode = Label(master=window,text="QR Code")
-lbl_qrCode.grid(row=3, column=0, sticky=W)
+lbl_qrCode.grid(row=6, column=1, sticky=W)
 
-txt_qrCode = Entry(window,width=50)
-txt_qrCode.grid(row=3, column=1)
+txt_qrCode = Entry(window,width=30)
+txt_qrCode.grid(row=7, column=1)
 
+###
+lbl_empty = Label(master=window,text="")
+lbl_empty.grid(row=8, column=0)
 
 ### Zeit
-lbl_zeit = Label(master=window,text="Datum/Uhrzeit")
-lbl_zeit.grid(row=4, column=0, sticky=W)
+bool_zeit = IntVar()
+chb_zeit = Checkbutton(master=window, variable=bool_zeit)
+chb_zeit.grid(row=9, column=0, sticky=W)
 
-txt_zeit = Entry(master=window,width=50)
-txt_zeit.grid(row=4, column=1)
-
-lbl_zeit = Label(master=window,text="02 FEB 2019 22:22:22")
-lbl_zeit.grid(row=5, column=1)
+lbl_zeitBool = Label(master=window,text="Datum/Uhrzeit einstellen?")
+lbl_zeitBool.grid(row=9, column=1, sticky=W)
 
 
+lbl_zeit = Label(master=window,text="Datum/Uhrzeit (02 FEB 2019 22:22:22)")
+lbl_zeit.grid(row=10, column=1, sticky=W)
+
+txt_zeit = Entry(master=window,width=30)
+txt_zeit.grid(row=11, column=1)
+
+
+###
 lbl_empty = Label(master=window,text="")
-lbl_empty.grid(row=6, column=0)
+lbl_empty.grid(row=12, column=0)
 
 ### Start
 global btn_start
 btn_start = ttk.Button(window,text='Starten', command=startScript)
-btn_start.grid(column=1,row=7, sticky=E)
+btn_start.grid(row=13, column=2, sticky=E)
 
 window.mainloop()
